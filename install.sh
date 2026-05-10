90fe88e38c1d93fa50dcdcb12c624ecc444a0d12
WhyNotSoGoodBitch <mninvisiblepvt@gmail.com>
2026-05-10 10:17:17 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y python3 python3-pip git curl tor
+pip3 install PySocks
+pip3 install requests[socks] proxy-rotator
 pip3 install requests fake-useragent beautifulsoup4
 sudo systemctl enable tor
 sudo systemctl start tor
